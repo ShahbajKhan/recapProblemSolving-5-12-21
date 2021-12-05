3683be1a76acd9bd26984050af24487e625fce54
ShahbajKhan <shktoky@gmail.com>
2021-12-05 23:06:46 +0600

    first commit

--- CentimeterToMeter.js
+++ CentimeterToMeter.js
@@ -1,10 +1,16 @@
-// 1 m = 100 cm
-// 1 cm = 1/100 m=0.01 m
-//2 cm = (0.01*2) = 0.02 m
-
-function centimeterToMeter(num){
-    var meter = 0.01 * 2;
-    return meter; 
+// 1 m =100 cm
+//  1 cm = 1/100 m = 0.01m
+//  2 cm = (0.01* 2)= 0.02 m
+// 0.01 * input
+function centimeterToMeter(inputLength){
+    if(inputLength < 0){
+        return "Length cannot be negative";
+    }
+    else{
+        var meterLength = 0.01* inputLength;
+        // console.log(meterLength)
+        return meterLength;
+    }
 }
-var result = centimeterToMeter(2);
-console.log(result+" meter");
+var result = centimeterToMeter(-17);
+console.log("from result " + result)
